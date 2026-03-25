74feb1dfafa97603b6a4b55742c36c12de7c4c7d
Alejandro <alejandrops32012@gmail.com>
2026-03-25 11:52:08 +0100

    Añadida función de login

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # crea las tablas
     app.run(debug=True)
+    
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
